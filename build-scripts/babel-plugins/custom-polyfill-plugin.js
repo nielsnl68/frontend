63524f98d478188498df2c65d547c71bb7166e82
Steve Repsher <steverep@users.noreply.github.com>
2024-05-01 18:17:04 +0000

    Inject element polyfills where used using Babel

--- build-scripts/babel-plugins/custom-polyfill-plugin.js
+++ build-scripts/babel-plugins/custom-polyfill-plugin.js
@@ -1,7 +1,44 @@
 import defineProvider from "@babel/helper-define-polyfill-provider";
+import { join } from "node:path";
+import paths from "../paths.cjs";
+
+const POLYFILL_DIR = join(paths.polymer_dir, "src/resources/polyfills");
 
 // List of polyfill keys with supported browser targets for the functionality
 const PolyfillSupport = {
+  "element-append": {
+    android: 54,
+    chrome: 54,
+    edge: 17,
+    firefox: 49,
+    ios: 10.0,
+    opera: 41,
+    opera_mobile: 41,
+    safari: 10.0,
+    samsung: 6.0,
+  },
+  "element-getattributenames": {
+    android: 61,
+    chrome: 61,
+    edge: 18,
+    firefox: 45,
+    ios: 10.3,
+    opera: 48,
+    opera_mobile: 45,
+    safari: 10.1,
+    samsung: 8.0,
+  },
+  "element-toggleattribute": {
+    android: 69,
+    chrome: 69,
+    edge: 18,
+    firefox: 63,
+    ios: 12.0,
+    opera: 56,
+    opera_mobile: 48,
+    safari: 12.0,
+    samsung: 10.0,
+  },
   fetch: {
     android: 42,
     chrome: 42,
@@ -33,7 +70,14 @@ const polyfillMap = {
     Proxy: { key: "proxy", module: "proxy-polyfill" },
     fetch: { key: "fetch", module: "unfetch/polyfill" },
   },
-  instance: {},
+  instance: {
+    ...Object.fromEntries(
+      ["append", "getAttributeNames", "toggleAttribute"].map((prop) => {
+        const key = `element-${prop.toLowerCase()}`;
+        return [prop, { key, module: join(POLYFILL_DIR, `${key}.ts`) }];
+      })
+    ),
+  },
   static: {},
 };
 
@@ -42,14 +86,16 @@ export default defineProvider(
   ({ createMetaResolver, debug, shouldInjectPolyfill }) => {
     const resolvePolyfill = createMetaResolver(polyfillMap);
     return {
-      name: "HA Custom",
+      name: "custom-polyfill",
       polyfills: PolyfillSupport,
       usageGlobal(meta, utils) {
         const polyfill = resolvePolyfill(meta);
         if (polyfill && shouldInjectPolyfill(polyfill.desc.key)) {
           debug(polyfill.desc.key);
           utils.injectGlobalImport(polyfill.desc.module);
+          return true;
         }
+        return false;
       },
     };
   }
